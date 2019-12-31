14e381a872b5e22bccf42d8e829948cd5c0177da
Janez Perme <discoversci@hotmail.com>
2019-12-31 10:33:45 +0100

    nove metode

--- QueueApplication01/QueueApplication01.cpp
+++ QueueApplication01/QueueApplication01.cpp
@@ -1,20 +1,23 @@
-// QueueApplication01.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <queue>
+#include <deque>
+#include <list>
+#include <vector>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int a1[] = { 1,2,3,4,5,6,7,8,9,10 };
+	//1. default constructor
+	queue <int> s1;
+	//2. copy constructor;
+	queue <int> s2(s1);
+	//3. initiilization using predefined container
+	deque <int> d1(a1, a1 + 10);
+	queue <int> s3(d1);
+
+	//4. using non-default storage
+	queue<int, list <int> > s4;
+	queue<int, vector <int> > s5;
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
